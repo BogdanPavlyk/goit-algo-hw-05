3443d9dc9ef6cc77955abc084d17f096f3b8ade5
Bogdan Pavlyk <bogdan.pavlyk7@gmail.com>
2024-03-24 16:24:34 +0000

    Improved hw_05_02

--- hw_05_02.py
+++ hw_05_02.py
@@ -1,28 +1,27 @@
-def binary_search(arr, target):
+def binary_search_with_upper_bound(arr, target):
+    left, right = 0, len(arr) - 1
     iterations = 0
-    low, high = 0, len(arr) - 1
     upper_bound = None
-
-    while low <= high:
-        mid = (low + high) // 2
-        mid_value = arr[mid]
+    
+    while left <= right:
         iterations += 1
-
-        if mid_value < target:
-            low = mid + 1
-        elif mid_value > target:
-            upper_bound = mid_value
-            high = mid - 1
+        mid = (left + right) // 2
+        
+        if arr[mid] < target:
+            left = mid + 1
         else:
-            upper_bound = mid_value
-            break
-
-    return iterations, upper_bound
+            upper_bound = arr[mid]
+            right = mid - 1
+    
+    if upper_bound is None:
+        return iterations, None  # Target is greater than any array element
+    else:
+        return iterations, upper_bound
 
 sorted_array = [1.2, 2.3, 3.4, 5.5, 8.8, 11.11, 13.13, 17.17, 19.19, 21.21]
 target_value = 20.1
 
-iterations, upper_bound = binary_search(sorted_array, target_value)
+iterations, upper_bound = binary_search_with_upper_bound(sorted_array, target_value)
 
 if upper_bound is not None:
     print(f"Елемент {target_value} знайдено за {iterations} ітерацій. Верхня межа: {upper_bound}")
